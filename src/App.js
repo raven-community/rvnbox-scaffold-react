989622bba7830225679197add12bdf5c628a7bab
MSFTserver <sawyerlemmo@gmail.com>
2019-01-03 13:54:47 -0800

    update to testnet & update packages

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ let mnemonic = RVNBOX.Mnemonic.generate(256, RVNBOX.Mnemonic.wordLists()[lang]);
 let rootSeed = RVNBOX.Mnemonic.toSeed(mnemonic);
 
 // master HDNode
-let masterHDNode = RVNBOX.HDNode.fromSeed(rootSeed, "ravencoin");
+let masterHDNode = RVNBOX.HDNode.fromSeed(rootSeed, "testnet");
 
 // HDNode of BIP44 account
 let account = RVNBOX.HDNode.derivePath(masterHDNode, "m/44'/175'/0'");
@@ -55,7 +55,7 @@ class App extends Component {
         }
 
         // instance of transaction builder
-        let transactionBuilder = new RVNBOX.TransactionBuilder("ravencoin");
+        let transactionBuilder = new RVNBOX.TransactionBuilder("testnet");
         // original amount of satoshis in vin
         let originalAmount = result[0].satoshis;
 
